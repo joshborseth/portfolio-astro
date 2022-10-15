c8a2496dd0494848e01da759a31f485906421d79
Joshua Borseth <joshuaborseth@Joshuas-Air.va.shawcable.net>
2022-10-15 13:43:01 -0700

    restructured files

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,4 @@
-import SkillIcon from "./SkillIcon";
+import { motion } from "framer-motion";
 import Typescript from "/typescript.svg";
 import React from "/react.svg";
 import Tailwind from "/tailwind.svg";
@@ -24,3 +24,21 @@ const Skills = () => {
 };
 
 export default Skills;
+
+type Props = {
+  delay: number;
+  src: string;
+  alt: string;
+};
+const SkillIcon = (props: Props) => {
+  return (
+    <motion.img
+      animate={{ opacity: 1, rotate: 360 }}
+      initial={{ opacity: 0, rotate: 180 }}
+      transition={{ duration: 0.25, delay: props.delay }}
+      className="bg-secondary rounded-lg"
+      src={props.src}
+      alt={props.alt}
+    />
+  );
+};
--- src/components/SocialMediaFooter.tsx
+++ src/components/SocialMediaFooter.tsx
@@ -2,7 +2,8 @@ import Email from "/email.svg";
 import Twitter from "/twitter.svg";
 import Github from "/github.svg";
 import LinkedIn from "/linkedin.svg";
-import SocialLink from "../footer/SocialLink";
+import { motion } from "framer-motion";
+
 const SocialMediaFooter = () => {
   return (
     <footer className="fixed z-50 bottom-0 h-24 w-full lg:w-1/2 transform -translate-x-1/2 left-1/2 bg-accent rounded-2xl">
@@ -25,3 +26,25 @@ const SocialMediaFooter = () => {
 };
 
 export default SocialMediaFooter;
+
+type Props = {
+  src: string;
+  alt: string;
+};
+const SocialLink = (props: Props) => {
+  return (
+    <a href="#">
+      <motion.img
+        src={props.src}
+        alt={props.alt}
+        whileHover={{
+          scale: 1.3,
+          y: -10,
+          transition: { duration: 0.1 },
+        }}
+        whileTap={{ scale: 0.9 }}
+        className="shadow-2xl"
+      />
+    </a>
+  );
+};
