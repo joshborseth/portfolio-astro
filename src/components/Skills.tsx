0fc7381229857e54f36578fa7258ebb98d356129
Joshua Borseth <joshuaborseth@Joshuas-Air.va.shawcable.net>
2022-10-15 18:06:31 -0700

    added favicon

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -33,8 +33,8 @@ type Props = {
 const SkillIcon = (props: Props) => {
   return (
     <motion.img
-      animate={{ opacity: 1, rotate: 360 }}
-      initial={{ opacity: 0, rotate: 180 }}
+      animate={{ opacity: 1, x: 0 }}
+      initial={{ opacity: 0, x: -100 }}
       transition={{ duration: 0.25, delay: props.delay }}
       className="bg-secondary rounded-lg"
       src={props.src}
